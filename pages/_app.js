c2d289953bbe4e03e1821586bb6e4276f21c51b2
JohannSiemens <johann.siemens85@gmx.de>
2023-05-19 14:11:42 +0200

    implemented search function against test-data & result overview

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import GlobalStyle from "../styles";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [exerciseInput, setExerciseInput] = useState();
+  const [exerciseResult, setExerciseResult] = useState([]);
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        exerciseInput={exerciseInput}
+        setExerciseInput={setExerciseInput}
+        exerciseResult={exerciseResult}
+        setExerciseResult={setExerciseResult}
+      />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,58 @@
-export default function HomePage() {
+import { testExercises } from "@/test_data";
+
+export default function HomePage({
+  exerciseInput,
+  setExerciseInput,
+  exerciseResult,
+  setExerciseResult,
+}) {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>My Workout</h1>
+      <ExerciseSearchForm
+        exerciseResult={exerciseResult}
+        setExerciseInput={setExerciseInput}
+        setExerciseResult={setExerciseResult}
+      />
+      <ExerciseResults exerciseResult={exerciseResult} />
     </div>
+  );
+}
+
+function ExerciseSearchForm({
+  setExerciseInput,
+  setExerciseResult,
+  exerciseResult,
+}) {
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    const exerciseInput = event.target.elements.exercise_search.value;
+    setExerciseInput(exerciseInput);
+    const testResult = testExercises.filter((exercise) =>
+      exercise.name.includes(exerciseInput)
+    );
+    setExerciseResult(testResult);
+    console.log(exerciseResult);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="exercise-search">
+        Exercise: <input type="text" name="exercise_search" required></input>
+      </label>
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+function ExerciseResults({ exerciseResult }) {
+  return (
+    <ul>
+      {exerciseResult.map((exercise) => (
+        <li key={exercise.name + exercise.type}>{exercise.name}</li>
+      ))}
+    </ul>
   );
 }
