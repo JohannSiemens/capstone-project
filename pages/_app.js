aae2b6ab9046b403758b09b3485d2e6a0c695e20
JohannSiemens <johann.siemens85@gmx.de>
2023-05-26 15:17:54 +0200

    changed fetcher to be more robust

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,17 @@ import GlobalStyle from "../styles";
 import { useState } from "react";
 import { SWRConfig } from "swr";
 
-const fetcher = (url) => fetch(url).then((response) => response.json());
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
+
+  return response.json();
+};
 
 export default function App({ Component, pageProps }) {
   const [exerciseResult, setExerciseResult] = useState([]);
