820e1bd063a6bfe93a02bb416ae8c227dd4d81fa
JohannSiemens <johann.siemens85@gmx.de>
2023-05-31 11:54:25 +0200

    implemented GET & POST Api for workouts & fetch for POST in NewWorkoutForm

--- components/WorkoutsList/NewWorkoutForm/index.js
+++ components/WorkoutsList/NewWorkoutForm/index.js
@@ -1,8 +1,26 @@
+import useSWR from "swr";
+
 export default function NewWorkoutForm() {
+  const { mutate } = useSWR("/api/workouts-db");
+  async function createWorkout(event) {
+    event.preventDefault();
+    const workout = event.target.elements.new_workout.value;
+    const response = fetch("/api/workouts-db", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(workout),
+    });
+    if (response.ok) {
+      mutate();
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={createWorkout}>
       <label>
-        Title:{" "}
+        Title:
         <input
           type="text"
           name="new_workout"
