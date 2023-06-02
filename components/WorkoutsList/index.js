5d91dd39ef2c6ad7f1cfaa8b0d1121a200d1066d
JohannSiemens <johann.siemens85@gmx.de>
2023-06-02 16:57:28 +0200

    refactored code based on PR feedback

--- components/WorkoutsList/index.js
+++ components/WorkoutsList/index.js
@@ -1,11 +1,10 @@
 import useSWR from "swr";
 import Link from "next/link";
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 export default function WorkoutsList() {
   const { data, isLoading, error, mutate } = useSWR("/api/workouts-db");
   const [isEdit, setIsEdit] = useState();
-  const inputRef = useRef(null);
 
   if (isLoading || error) {
     return <h1>Loading...</h1>;
@@ -28,8 +27,9 @@ export default function WorkoutsList() {
     }
   }
 
-  async function editWorkout(id) {
-    const title = inputRef.current.value;
+  async function editWorkout(event, id) {
+    event.preventDefault();
+    const title = event.target.elements.newWorkout.value;
     const response = await fetch(`/api/workouts-db/${id}`, {
       method: "PUT",
       headers: {
@@ -52,24 +52,29 @@ export default function WorkoutsList() {
       {data.map((workout) => (
         <li key={workout._id}>
           {workout._id === isEdit ? (
-            <input
-              type="text"
-              name="new_workout"
-              id="new_workout"
-              pattern="[A-Za-z]{1,20}"
-              defaultValue={workout.title}
-              ref={inputRef}
-              required
-            ></input>
+            <form
+              onSubmit={(event) => {
+                editWorkout(event, workout._id);
+              }}
+            >
+              <label>
+                Name:
+                <input
+                  type="text"
+                  name="newWorkout"
+                  id="newWorkout"
+                  pattern="[A-Za-z]{1,20}"
+                  defaultValue={workout.title}
+                  required
+                />
+              </label>
+              {workout._id === isEdit && <button type="submit">Submit</button>}
+            </form>
           ) : (
             <Link href={`/workout/${workout._id}`}>{workout.title}</Link>
           )}
           <button onClick={() => deleteWorkout(workout._id)}>Delete</button>
-          {workout._id === isEdit ? (
-            <button type="submit" onClick={() => editWorkout(workout._id)}>
-              Submit
-            </button>
-          ) : (
+          {workout._id !== isEdit && (
             <button onClick={() => isEditModeSetter(workout._id)}>Edit</button>
           )}
         </li>
