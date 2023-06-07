60e266171962d2af6361004cf28a90b7f18e0a70
JohannSiemens <johann.siemens85@gmx.de>
2023-06-07 16:12:23 +0200

    changed Form component based on PR feedback

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,20 +1,10 @@
 import styled from "styled-components";
 
-const SmallForm = styled.form`
+const Form = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
-
-const Form = ({ children, variant, component, ...props }) => {
-  if (variant === "small") {
-    return (
-      <SmallForm as={component} {...props}>
-        {children}
-      </SmallForm>
-    );
-  }
-};
 
 export default Form;
