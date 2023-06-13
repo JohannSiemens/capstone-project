9c232cfc6725175bcf420bcf40158a62d0bda144
JohannSiemens <johann.siemens85@gmx.de>
2023-06-13 12:36:10 +0200

    refactored added exercise list overview

--- components/AddedExercisesList/AddedExercise/openCard/index.js
+++ components/AddedExercisesList/AddedExercise/openCard/index.js
@@ -1,9 +1,11 @@
 import OpenCardForm from "./OpenCardForm";
 import AddedSets from "./AddedSets";
+import Button from "@/components/Button";
 
-export default function OpenCard({ id }) {
+export default function OpenCard({ id, deleteExercise }) {
   return (
     <>
+      <Button onClick={() => deleteExercise(exercise._id)}>Delete</Button>
       <OpenCardForm id={id} />
       <AddedSets id={id} />
     </>
