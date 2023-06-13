3453aa7ebd3cffba3d6108bdbadac0de3d0fb48b
JohannSiemens <johann.siemens85@gmx.de>
2023-06-13 15:26:08 +0200

    refactoring of exercise list components

--- components/AddedExercisesList/AddedExercise/openCard/index.js
+++ components/AddedExercisesList/AddedExercise/openCard/index.js
@@ -1,13 +1,14 @@
 import OpenCardForm from "./OpenCardForm";
 import AddedSets from "./AddedSets";
 import Button from "@/components/Button";
+import Wrapper from "@/components/Wrapper";
 
 export default function OpenCard({ id, deleteExercise }) {
   return (
     <>
-      <Button onClick={() => deleteExercise(exercise._id)}>Delete</Button>
       <OpenCardForm id={id} />
       <AddedSets id={id} />
+      <Button onClick={() => deleteExercise(id)}>Delete Exercise</Button>
     </>
   );
 }
