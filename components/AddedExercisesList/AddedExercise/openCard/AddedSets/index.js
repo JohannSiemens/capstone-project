55b562367c2c1086e8266f42d62523562f3e4844
JohannSiemens <johann.siemens85@gmx.de>
2023-06-14 12:46:26 +0200

    refactored weight to be only displayed when there is actually weight to be displayed

--- components/AddedExercisesList/AddedExercise/openCard/AddedSets/index.js
+++ components/AddedExercisesList/AddedExercise/openCard/AddedSets/index.js
@@ -120,8 +120,8 @@ function AddedSetsOverview({ set, index, deleteSet, editModeSetter, dataID }) {
   return (
     <Wrapper variant="column">
       <Typography variant="text">
-        Set {index + 1} <br /> Repetitions: {set.repetitions} Weight:{" "}
-        {set.weight}kg
+        Set {index + 1} <br /> Repetitions: {set.repetitions}{" "}
+        {set.weight > 0 && `Weight: ${set.weight} kg`}
       </Typography>
       <Wrapper variant="row">
         <Button onClick={() => deleteSet(dataID, set.id)}>Delete</Button>
